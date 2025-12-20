da5fcaa3804654ececef909e82c3f0af5842dfbe
anuragpapineni <anuragpapineni@gmail.com>
2025-12-20 15:58:32 -0800

    Revert "Merge branch 'master' into main"
    This reverts commit 57dac1736790bf16162649d57bb5f32b48bebe72, reversing
    changes made to 848e232171f51ad9911f96be4519bea8ee8128d3.

--- Source/rhythmActionGame/rhythmActionGame.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "rhythmActionGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, rhythmActionGame, "rhythmActionGame" );
-
-DEFINE_LOG_CATEGORY(LogrhythmActionGame)
