99b8f31890b6c748ca6c01f2ba5a366b2f5b4db1
anandpapineni <anandpapineni9@gmail.com>
2025-12-20 13:37:01 -0600

    Initial commit
    Signed-off-by: anandpapineni <anandpapineni9@gmail.com>

---
+++ Source/rhythmActionGame/rhythmActionGame.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "rhythmActionGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, rhythmActionGame, "rhythmActionGame" );
+
+DEFINE_LOG_CATEGORY(LogrhythmActionGame)
